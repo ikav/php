6261922acb38c48826810befad7a04a4168b4f91
ikav <ikav@mail.ru>
2018-10-11 23:48:59 +0500

    Реализована калькулятор с кнопками и оптимизирован калькулятор с select

--- 06/calculator.php
+++ 06/calculator.php
@@ -1,3 +1,7 @@
+<?php
+  include "server.php";
+?>
+
 <form>
   
   <h1>Калькулятор</h1>
@@ -13,9 +17,22 @@
     <option <?=$selected_divided?> value="/">/</option>
   </select>
   <input type="text" name="secondField" value="<?=$_GET['secondField']?>" style="width:100px">
-  <?php
-    include_once "server.php";
-  ?>
-  <p><input type="submit" value="Выполнить"></p>
+  <?="$res"?>
+
+  <p><input type="submit" name="calculate" value="Выполнить"></p>
+  
+  <hr>
+  
+  <!--Калькулятор с кнопками-->
+  <p>Введите числа и выберите операцию. Сразу же получите результат</p>
+  <input type="text" name="firstNum" value="<?=$_GET['firstNum']?>" style="width:100px">
+  <input type="text" name="secondNum" value="<?=$_GET['secondNum']?>" style="width:100px">
+  <?="$res2"?>
+  <p>
+    <input type="submit" name="button" value="+">
+    <input type="submit" name="button" value="-">
+    <input type="submit" name="button" value="*">
+    <input type="submit" name="button" value="/">
+  </p>
   
 </form>
