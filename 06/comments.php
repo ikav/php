4d7377b36231db76ab5a83dc8e1ab72b953cea0d
ikav <ikav@mail.ru>
2018-10-13 22:37:56 +0500

    Доработал задание 3 (вывод комментариев). Сделал чтобы форма повторно не посылалась по F5 и несколько комментов одинаковых не добавлялись

--- 06/comments.php
+++ 06/comments.php
@@ -1,3 +1,9 @@
+<?php 
+  if (!empty($_POST["saveComment"])) {
+    header("Location: ".$_SERVER["REQUEST_URI"]);
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,18 +13,21 @@
   <title>Комментарии</title>
 </head>
 <body>
+
   <h1>Комментарии к нашей работе</h1>
-  
+
   <?php
     include_once "/engine/outputComments.php";
     include_once "/engine/insertComment.php";
   ?>
-  
+
+  <?=$message?>
   <form method="POST" action="comments.php">
     <h2>Оставьте свой комментарий</h2>
     <p><input placeholder="Скажите как Вас зовут" type="text" name="author" style="width: 250px" required=""></p>
     <textarea placeholder="Введите свой комментарий" name="comment" rows="10" cols="50" required=""></textarea>
     <p><input type="submit" name="saveComment" value="Сохранить"></p>
   </form>
+  
 </body>
 </html>
